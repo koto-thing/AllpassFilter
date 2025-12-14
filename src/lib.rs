c7dfff92d0180c209a45443a5383c6eff3d74fb3
koto-thing <gotoukenta62@gmail.com>
2025-12-15 01:50:44 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+
+pub mod delay;
+pub mod allpass;
+pub mod interpolation;
+pub mod parameter;
+
+pub use delay::DelayLine;
+pub use allpass::AllPassFilter;
+pub use interpolation::{Interpolator, Linear, Nearest, Cubic};
+pub use parameter::SmoothedParam;
